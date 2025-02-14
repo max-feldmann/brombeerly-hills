a42b1992751ee65b65774638c8cb7ca78760781b
Max <max@space-4.de>
2025-02-14 08:43:13 +0100

    RESPONSIVE optimisation of grid section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,18 @@ export default function Home() {
     <>
       <DroneflightHero />
 
-      <section className="border-b border-gray-700">
-        <div className="container mx-auto">
-          <div className="grid grid-cols-2">
-            <div className="col-span-2 lg:col-span-1 border-r border-gray-700">
-              <div className="grid grid-cols-2">
-                <div className="col-span-2 lg:col-span-1 px-4 py-20 pr-4">
-                  <Headline as="h2">Das Paradies geht verloren!</Headline>
+      <section className="">
+        <div className="">
+          <div className="grid grid-cols-2 grid-rows-3">
+
+            <div className="col-span-2 lg:col-span-1 row-span-1 | flex justify-end | border-b border-r border-gray-700">
+              <div className="grid grid-cols-2 " style={{ maxWidth: "752px"}}>
+                <div className="ml-4 xl:ml-0 py-20 pr-4 | col-span-2 lg:col-span-1" >
+                  <Headline as="h2">1 Das Paradies geht verloren!</Headline>
                   <Paragraph>Unsere heimischen Streuobstwiesen, ein enorm wichtiges Ökosystem, sind vom Aussterben bedroht.</Paragraph>
                   <Link href="/" className="btn btn-outline btn-sm btn-primary">Warum das so ist</Link>
                 </div>
-                <div className="col-span-2 lg:col-span-1 border-l border-gray-700 relative">
+                <div className="col-span-2 lg:col-span-1 relative">
                   <div
                     className='
                     bg-gradient-to-b from-base-100/90 via-base-100/70 to-base-100/15 
@@ -29,10 +30,28 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="px-4 py-20 md:p-10 lg:p-20 col-span-2 lg:col-span-1">
-              <Headline as="h2">Ich bin eine Headline zweiter Rangordnung. Du wirst mich weiter unten finden.</Headline>
-              <Paragraph>Ja hier wird wsl. auch text stehen oder so.</Paragraph>
+
+            <div className="px-4 py-20 md:p-10 lg:p-20 | col-span-2 lg:col-span-1 row-span-1 lg:row-span-2 | border-b lg:border-b-0 border-gray-700">
+              <div style={{ maxWidth: "752px"}}>
+                <Headline as="h2">2 Ich bin eine Headline zweiter Rangordnung. Du wirst mich weiter unten finden.</Headline>
+                <Paragraph>Ja hier wird wsl. auch text stehen oder so. Zumindest würde ich sagen ist das basierend auf extrem plausiblen Erfahrungen der Vergangenheit sehr wahrscheinlich.</Paragraph>
+              </div>
             </div>
+
+            <div className="col-span-2 lg:col-span-1 row-span-1 lg:row-span-2 | border-r-0 lg:border-r border-b  border-gray-700 | flex justify-end">
+              <div className="px-4 xl:px-0 py-20" style={{ maxWidth: "752px"}}>
+                <Headline as="h2">3 Ich bin eine Headline zweiter Rangordnung. Du wirst mich weiter unten finden.</Headline>
+                <Paragraph>Ja hier wird wsl. auch text stehen oder so. Zumindest würde ich sagen ist das basierend auf extrem plausiblen Erfahrungen der Vergangenheit sehr wahrscheinlich.</Paragraph>
+              </div>
+            </div>
+
+            <div className="col-span-2 lg:col-span-1 row-span-1 | flex flex-col items-start | border-b border-t-0 lg:border-t border-gray-700">
+              <div className="px-4 py-20 md:p-10 lg:p-20" style={{ maxWidth: "752px"}}>
+                <Headline as="h2">4 Hier au noch Content hin</Headline>
+                <Paragraph>Lets go Content writing whoop whoop</Paragraph>                
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
