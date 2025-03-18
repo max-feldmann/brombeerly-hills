9414465ec748b210d4268c430f5f47aa014310fa
Max <max@space-4.de>
2025-03-18 08:16:37 +0100

    Removed deprecated daisy config from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,9 @@
 import type { Config } from "tailwindcss";
-import daisyui from 'daisyui'
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
-  plugins: [daisyui],
-
-  daisyui: {
-    themes: ["light", "dark", "forest"],
-  },
+  ]
 } satisfies Config;
