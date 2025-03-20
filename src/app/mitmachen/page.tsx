122ef444aa9bb9dfa1757a533152a4d5a2c457bd
Max <max@space-4.de>
2025-03-20 09:29:30 +0100

    CONT

--- src/app/mitmachen/page.tsx
+++ src/app/mitmachen/page.tsx
@@ -10,17 +10,19 @@ const page = () => {
     <>
         <section className='mb-20'>
             <div className='container mx-auto lg:my-28 border-b lg:border border-gray-700'>
-                <div className="p-6 md:p-10 lg:p-20 flex flex-col items-start lg:items-center">
-                    <span className={`${cormorantGaramond.className} mb-8 bg-primary/10 rounded-lg font-semibold py-2 px-5`}>Aktiver Naturschutz</span>
-                    <Headline as="h1">Hilf uns ein Stück Kulturlandschaft zu erhalten!</Headline>
-                    <Paragraph>Bei unserem Projekt gibt es jede Menge zu tun! Von Bäumebeschneiden bis Bodenbearbeitung - Wir freuen uns über helfende Hände.</Paragraph>
-                    <Image
-                        src={'/image/test.png'}
-                        width={1920}
-                        height={1080}
-                        alt="Demo"
-                        className="lg:w-1/3"
-                        />
+                <div className="p-6 md:p-10 lg:p-20 | flex flex-col lg:items-center items-start">
+                    <div className='max-w-2/3 flex flex-col items-start lg:items-center'>
+                        <span className={`${cormorantGaramond.className} bg-primary/10 rounded-lg font-semibold py-2 px-5`}>Aktiver Naturschutz</span>
+                        <Headline as="h1" className='mt-8'>Hilf uns ein Stück Kulturlandschaft zu erhalten!</Headline>
+                        <Paragraph>Gemeinsam restaurieren wir eine alte Streuobstwiese in Bamberg. Bei unserem Projekt gibt es jede Menge zu tun! Von Bäumebeschneiden bis Bodenbearbeitung - Wir freuen uns über helfende Hände.</Paragraph>
+                        <Image
+                            src={'/image/test.png'}
+                            width={1920}
+                            height={1080}
+                            alt="Demo"
+                            className="lg:w-2/3"
+                            />
+                    </div>
                 </div>
                 <div className="grid grid-cols-3">
                     <div className="col-span-3 lg:col-span-1 | lg:border-r border-gray-700">
