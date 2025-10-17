0128ae9c09ecc173eb43862883ee5833b7ee7c77
Feldmanovitch <max@space-4.de>
2025-10-17 13:13:08 +0200

    Added logo

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,7 +7,14 @@ const Navbar = () => {
     <header className='z-30 border-b bg-base-100/30 backdrop-blur-lg border-gray-700 sticky top-0'>
       <div className="navbar  container mx-auto">
         <div className="navbar-start">
-          <Link href="/" className="btn btn-ghost text-xl">Brombeerly Hills</Link>
+          <Link href="/" className="btn btn-ghost text-xl">
+            <Image
+              src="/image/brand/logo.svg"
+              alt="Brombeerly Hills Logo"
+              width={250}
+              height={250}
+            />
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
